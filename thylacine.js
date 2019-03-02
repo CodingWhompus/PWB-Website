eb3f1a60d153cda47eeec219f7cb07e1bc1b9358
Caddywhompus <roswellington@outlook.com>
2019-03-02 12:40:29 -0600

    we will find the thylacines

---
+++ thylacine.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+function yesText() {
+    alert("Well you're wrong but go off I guess")
+}
+function noText() {
+    alert("You have the heart of a true adventurer!")
+}
+
+let yesButton = document.getElementById("yes")
+yesButton.addEventListener('click', function () {
+    yesText()
+})
+let noButton = document.getElementById("no")
+noButton.addEventListener('click', function () {
+    noText()
+})
+
+
+
